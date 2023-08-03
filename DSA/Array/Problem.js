dc1020799cb25e1388368210618cff3081ad11cb
rkshawon <rkshawon16@gmail.com>
2023-08-03 15:05:13 +0600

    added dsa

---
+++ DSA/Array/Problem.js
@@ -0,0 +1,7 @@
+const problem = [
+  {
+    _1672: "https://leetcode.com/problems/richest-customer-wealth/",
+    _1295:
+      "https://leetcode.com/problems/find-numbers-with-even-number-of-digits/",
+  },
+];
