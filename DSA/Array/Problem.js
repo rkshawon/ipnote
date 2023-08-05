32bcea937e7de07f17badbfe20f0b8fa492cabd9
rkshaown <rkshawon36@gamil.com>
2023-08-05 21:52:51 +0600

    updated

--- DSA/Array/Problem.js
+++ DSA/Array/Problem.js
@@ -5,5 +5,6 @@ const problem = [
       "https://leetcode.com/problems/find-numbers-with-even-number-of-digits/",
     _744: "https://leetcode.com/problems/find-smallest-letter-greater-than-target/",
     _34: "https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/",
+    _852: "https://leetcode.com/problems/peak-index-in-a-mountain-array/",
   },
 ];
