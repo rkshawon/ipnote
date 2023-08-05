1ba8a41ae6d5defb7fc4cd6411bbbfa837dc77f5
rkshaown <rkshawon36@gamil.com>
2023-08-05 23:25:02 +0600

    updated

--- DSA/Array/Problem.js
+++ DSA/Array/Problem.js
@@ -1,11 +1,10 @@
 const problem = [
   {
-    _1672: "https://leetcode.com/problems/richest-customer-wealth/",
-    _1295:
-      "https://leetcode.com/problems/find-numbers-with-even-number-of-digits/",
-    _744: "https://leetcode.com/problems/find-smallest-letter-greater-than-target/",
-    _34: "https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/",
-    _852: "https://leetcode.com/problems/peak-index-in-a-mountain-array/",
+    1672: "https://leetcode.com/problems/richest-customer-wealth/",
+    1295: "https://leetcode.com/problems/find-numbers-with-even-number-of-digits/",
+    744: "https://leetcode.com/problems/find-smallest-letter-greater-than-target/",
+    34: "https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/",
+    852: "https://leetcode.com/problems/peak-index-in-a-mountain-array/",
     1095: "https://leetcode.com/problems/find-in-mountain-array/",
   },
 ];
