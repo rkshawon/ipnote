8490d76027bceacb202b28adf4676553e2eef2df
rkshawon <rkshawon16@gmail.com>
2023-08-06 16:41:14 +0600

    updated

--- DSA/Questions/array.java
+++ DSA/Questions/array.java
@@ -28,3 +28,33 @@ public class MyClass {
         return res;
     }
 }
+
+// Number of time array rotated using binary search
+
+public class MyClass {
+    public static void main(String args[]) {
+        int[] array = {4,5,6,0,1,2,3};
+        int pivot = findPivot(array);
+        System.out.println(pivot + 1);
+    }
+    static int findPivot (int[] array,){
+        int start = 0, end = array.length-1;
+        int res = -1;
+       while(start <= end){
+        int mid = start + (end - start) / 2;
+        if(array[mid] > array[mid + 1]){
+            return res = mid;
+        }
+        if(array[mid] < array[mid - 1]){
+            return res = mid - 1;
+        }
+        if(array[mid] < array[end]){
+            end = mid - 1;
+        }
+        if(array[mid] > array[start]){
+            start = mid + 1;
+        }
+       }
+        return res;
+    }
+}
