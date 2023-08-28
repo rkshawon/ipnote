d2f00696974ff55cdec71c2401c1a7bfcf1c8b43
rkshawon <rkshawon16@gmail.com>
2023-08-28 19:07:04 +0600

    983

--- DSA/Array/Problem.js
+++ DSA/Array/Problem.js
@@ -16,5 +16,6 @@ const problem = [
     41: "https://leetcode.com/problems/first-missing-positive/submissions/",
     832: "https://leetcode.com/problems/flipping-an-image/description/",
     215: "https://leetcode.com/problems/kth-largest-element-in-an-array/description/",
+    983: "https://leetcode.com/problems/minimum-cost-for-tickets/description/",
   },
 ];
