e6f7c190f375f227160630205ade5d27672c7694
rkshawon <rkshawon16@gmail.com>
2023-10-10 12:12:43 +0600

    u

--- DSA/index.java
+++ DSA/index.java
@@ -17,3 +17,14 @@ class Solution {
         }
     }
 }
+//https://leetcode.com/problems/validate-binary-search-tree/
+public class Solution {
+    public boolean isValidBST(TreeNode root, long minVal, long maxVal) {
+        if (root == null) return true;
+        if (root.val >= maxVal || root.val <= minVal) return false;
+        return isValidBST(root.left, minVal, root.val) && isValidBST(root.right, root.val, maxVal);
+    }
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+}
