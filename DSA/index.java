f4c2f88b93ee48f34814c2d4948e64b8416b5b01
rkshawon <rkshawon16@gmail.com>
2023-10-11 12:36:34 +0600

    u

--- DSA/index.java
+++ DSA/index.java
@@ -45,3 +45,16 @@ public class Codec {
         return builder(list);
     }
 }
+//https://leetcode.com/problems/path-sum/
+class Solution {
+    public boolean target(TreeNode root, int sum) {
+        if(root == null ) return false;
+        if(root.val == sum && root.left == null && root.right == null) {
+            return true;
+        }
+        return  target(root.left, sum-root.val) || target(root.right, sum-root.val);
+    }
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        return target(root, targetSum);
+    }
+}
