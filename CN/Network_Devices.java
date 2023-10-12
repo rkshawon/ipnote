18a6a12f59378c18321d68a4713181ccbda6e303
rkshawon <rkshawon16@gmail.com>
2023-10-12 12:24:30 +0600

    u

---
+++ CN/Network_Devices.java
@@ -0,0 +1,61 @@
+const Host?
+Host is any device that send and recieve traffic. Anything that send and recieve traffic over network.
+Example computet laptop phone  server printer cloud server 
+
+Client vs server : Client send request and server send response.relative to specific communication.
+
+Server is a computer with software installed which response to specific request
+
+
+const IP address?
+IP address stands for internet protocol address.
+An Internet Protocol (IP) address is a unique numerical identifier for every device or
+network that connects to the internet.
+IPv4 is 32 bits. IPv6 is 64 bits
+
+const Computer Network?
+A computer network is a system that connects two or more computing devices for transmitting and sharing information.
+Anytime two device is conncected, there establish network.
+
+
+const Repeater?
+A repeater is a networking device used to extend the reach or range of a network by amplifying and
+retransmitting data signals.
+It operates at the physical layer of the OSI model, primarily working with electrical or optical signals,
+and its main purpose is to regenerate and boost these signals so that they can travel over longer distances
+without signal degradation
+
+const Hub ?
+A hub is a physical layer networking device which is used to connect multiple devices in a network.
+A hub has many ports in it. A computer which intends to be connected to the network is plugged in to one of these ports.
+When a data frame arrives at a port, it is broadcast to every other port,
+without considering whether it is destined for a particular destination or not.
+
+
+const Bridge?
+A bridge in computer networking is a device that connects and filters traffic between two or more network segments,
+operating at the data link layer (Layer 2) of the OSI model. Its primary function is to intelligently forward data
+packets based on their MAC addresses, thereby reducing network collisions and improving network efficiency by
+creating separate collision domains. Bridges are used for segmenting and enhancing the performance of networks.
+
+const Switch?
+In computer networking, a switch is a network device that operates at the data link layer (Layer 2)
+of the OSI model.It is designed to connect multiple devices within a local area network (LAN) and
+intelligently manage the traffic flow.
+
+const Routers? 
+A router in computer networking is a device that connects multiple computer networks together and
+directs data traffic between them.Routers operate at the network layer (Layer 3) of the OSI model
+and are essential for forwarding data packets between different networks
+* Routers learn which network they are attach to known as Routes, stored in a routing table.
+Routing table stores all network which are connected.
+Default Gateway is a Router.
+
+
+const APRS ?
+ARP allows devices to find each others physical addresses within the same network. 
+
+const subnet mask?
+A subnet mask is a 32-bit address that is used in IP (Internet Protocol) networking to divide an
+IP address into network and host portions. It is used to identify which part of an IP address belongs
+to the network and which part belongs to the host.
