06cd34a4b2c300d5805c5db03b9224ce4eabf0e6
rkshawon <rkshawon16@gmail.com>
2023-12-07 12:52:30 +0600

    updated

--- DSA/index.java
+++ DSA/index.java
@@ -21,3 +21,29 @@ class Solution {
         return result;
     }
 }
+
+https://leetcode.com/problems/combinations/
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+        combineHelper(n, k, 1, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void combineHelper(int n, int k, int start, List<Integer> current, List<List<Integer>> result) {
+        if (k == 0) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            current.add(i);
+            combineHelper(n, k - 1, i + 1, current, result);
+            current.remove(current.size() - 1);
+        }
+    }
+}
