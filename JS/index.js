cd71c84c84110090565cf46ed1241ec35dc295b8
rkshawon <rkshawon36@gmail.com>
2024-03-08 19:00:09 +0600

    added this keyword

--- JS/index.js
+++ JS/index.js
@@ -1,2 +1,13 @@
-"use strict";
-console.log(this);
+var User = function () {};
+
+User.prototype.attributes = {
+  isAdmin: false,
+};
+
+var admin = new User("Sam"),
+  guest = new User("Bob");
+
+admin.attributes.isAdmin = true;
+
+console.log(admin.attributes.isAdmin);
+console.log(guest.attributes.isAdmin);
