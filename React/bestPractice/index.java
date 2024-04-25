c6f5979dacc70df8eeb51771ac043fed26d831c4
shawon <rabby.shawon@vivasoftltd.com>
2024-04-25 14:47:43 +0600

    Updated react folder

--- React/bestPractice/index.java
+++ React/bestPractice/index.java
@@ -1,1 +1,17 @@
-React Best Practice.
+<input onClick={handleEvent(e, 1)} />
+<input onClick={handleEvent(e, 2)} />
+<input onClick={handleEvent(e, 33)} />
+
+handleEvent(e, v){
+    //do something with e and v
+}
+if we need handleEvent in multiple place with different value we can we something like function currying.
+handleEvent(v){
+   return (e)=>{
+    //do something with e and v
+    }
+}
+handleEvent(1)
+handleEvent(2)
+handleEvent(3)
+
