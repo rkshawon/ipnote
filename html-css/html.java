c651bb1ca70333da1b813581665a1939847a853c
shawon <rabby.shawon@vivasoftltd.com>
2024-05-13 11:10:16 +0600

    Added resource to html-css folder

--- html-css/html.java
+++ html-css/html.java
@@ -1,1 +1,4 @@
 1 Rules - https://www.freecodecamp.org/news/html-best-practices/
+2 Google https://google.github.io/styleguide/htmlcssguide.html
+3 Html Css https://cssguidelin.es/#the-openclosed-principle
+4 Book https://theswissbay.ch/pdf/Books/Computer%20science/O%27Reilly/little-book-html-css-coding-guidelines.pdf
